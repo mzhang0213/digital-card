29f80442845c95fcb36e9bd406489d1057d33438
Michael Zhang <zhang.michael1@northeastern.edu>
2025-10-26 17:02:03 -0400

    fail

--- src/components/InvitationCard/GlitterEffect.tsx
+++ src/components/InvitationCard/GlitterEffect.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState, useCallback } from 'react';
 
 interface GlitterParticle {
--- src/components/InvitationCard/InvitationCard.tsx
+++ src/components/InvitationCard/InvitationCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useCallback } from 'react';
 import { InvitationConfig, CardState } from './types';
 import { DEFAULT_CONFIG } from './constants';
